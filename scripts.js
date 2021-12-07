0533ac25381f1ca3d5c47e28a07a6bb5ac6f593e
Saskia <saskia.slater@wsu.edu>
2021-12-07 20:52:14 +0000

    alternating x and o's javascript

--- scripts.js
+++ scripts.js
@@ -1,78 +1,245 @@
-// borrowing window.onload and .onclick code from TP6
+turn = 1;
+a1filled = false;
+b1filled = false;
+c1filled = false;
+a2filled = false;
+b2filled = false;
+c2filled = false;
+a3filled = false;
+b3filled = false;
+c3filled = false;
 window.onload = function() {
 
-	// add a click function to the <div> with id="a1"
-	document.querySelector("#a1").onclick = function() {
-		
-		// set the <div>'s inner <img> source to the O image, then make it display visibly 
-		document.querySelector("#a1 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
-		document.querySelector("#a1 img").style.display = "block";
-		
-	} // end #a1 onclick function
+  // add a click function to the <div> with id="a1"
+
+
+
+  document.querySelector("#a1").onclick = function() {
+
+  if (a1filled == false) {
+    
+    if (turn == 1) {
+      // set the <div>'s inner <img> source to the O image, then make it display visibly 
+      document.querySelector("#a1 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
+      document.querySelector("#a1 img").style.display = "block";
+      turn = 2;
+      a1filled = true;
+
+    } else {
+      document.querySelector("#a1 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
+      document.querySelector("#a1 img").style.display = "block";
+      turn = 1;
+    }
+    
+  } else {
+    
+  }
+ 
+} // end #a1 onclick function
+  
+  
+ 
   
   document.querySelector("#b1").onclick = function() {
-		
-		// set the <div>'s inner <img> source to the O image, then make it display visibly 
-		document.querySelector("#b1 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
-		document.querySelector("#b1 img").style.display = "block";
-		
-	}
+
+  if (b1filled == false) {
+    
+    if (turn == 1) {
+      // set the <div>'s inner <img> source to the O image, then make it display visibly 
+      document.querySelector("#b1 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
+      document.querySelector("#b1 img").style.display = "block";
+      turn = 2;
+      b1filled = true;
+
+    } else {
+      document.querySelector("#b1 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
+      document.querySelector("#b1 img").style.display = "block";
+      turn = 1;
+    }
+    
+  } else {
+    
+  }
+ 
+}
+  
   
   document.querySelector("#c1").onclick = function() {
-		
-		// set the <div>'s inner <img> source to the O image, then make it display visibly 
-		document.querySelector("#c1 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
-		document.querySelector("#c1 img").style.display = "block";
-		
-	}
+
+  if (c1filled == false) {
+    
+    if (turn == 1) {
+      // set the <div>'s inner <img> source to the O image, then make it display visibly 
+      document.querySelector("#c1 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
+      document.querySelector("#c1 img").style.display = "block";
+      turn = 2;
+      c1filled = true;
+
+    } else {
+      document.querySelector("#c1 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
+      document.querySelector("#c1 img").style.display = "block";
+      turn = 1;
+    }
+    
+  } else {
+    
+  }
+ 
+}
+  
+  
+  
   
   document.querySelector("#a2").onclick = function() {
-		
-		// set the <div>'s inner <img> source to the O image, then make it display visibly 
-		document.querySelector("#a2 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
-		document.querySelector("#a2 img").style.display = "block";
-		
-	}
+
+  if (a2filled == false) {
+    
+    if (turn == 1) {
+      // set the <div>'s inner <img> source to the O image, then make it display visibly 
+      document.querySelector("#a2 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
+      document.querySelector("#a2 img").style.display = "block";
+      turn = 2;
+      a2filled = true;
+
+    } else {
+      document.querySelector("#a2 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
+      document.querySelector("#a2 img").style.display = "block";
+      turn = 1;
+    }
+    
+  } else {
+    
+  }
+ 
+}
+  
+  
+  
+  
   
   document.querySelector("#b2").onclick = function() {
-		
-		// set the <div>'s inner <img> source to the O image, then make it display visibly 
-		document.querySelector("#b2 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
-		document.querySelector("#b2 img").style.display = "block";
-		
-	}
+
+  if (b2filled == false) {
+    
+    if (turn == 1) {
+      // set the <div>'s inner <img> source to the O image, then make it display visibly 
+      document.querySelector("#b2 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
+      document.querySelector("#b2 img").style.display = "block";
+      turn = 2;
+      b2filled = true;
+
+    } else {
+      document.querySelector("#b2 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
+      document.querySelector("#b2 img").style.display = "block";
+      turn = 1;
+    }
+    
+  } else {
+    
+  }
+ 
+}
   
+  
+  
+    
   document.querySelector("#c2").onclick = function() {
-		
-		// set the <div>'s inner <img> source to the O image, then make it display visibly 
-		document.querySelector("#c2 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
-		document.querySelector("#c2 img").style.display = "block";
-		
-	}
+
+  if (c2filled == false) {
+    
+    if (turn == 1) {
+      // set the <div>'s inner <img> source to the O image, then make it display visibly 
+      document.querySelector("#c2 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
+      document.querySelector("#c2 img").style.display = "block";
+      turn = 2;
+      c2filled = true;
+
+    } else {
+      document.querySelector("#c2 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
+      document.querySelector("#c2 img").style.display = "block";
+      turn = 1;
+    }
+    
+  } else {
+    
+  }
+ 
+}
   
+  
+  
+  
+    
   document.querySelector("#a3").onclick = function() {
-		
-		// set the <div>'s inner <img> source to the O image, then make it display visibly 
-		document.querySelector("#a3 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
-		document.querySelector("#a3 img").style.display = "block";
-		
-	}
+
+  if (a3filled == false) {
+    
+    if (turn == 1) {
+      // set the <div>'s inner <img> source to the O image, then make it display visibly 
+      document.querySelector("#a3 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
+      document.querySelector("#a3 img").style.display = "block";
+      turn = 2;
+      a3filled = true;
+
+    } else {
+      document.querySelector("#a3 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
+      document.querySelector("#a3 img").style.display = "block";
+      turn = 1;
+    }
+    
+  } else {
+    
+  }
+ 
+}
+
+  
   
   document.querySelector("#b3").onclick = function() {
-		
-		// set the <div>'s inner <img> source to the O image, then make it display visibly 
-		document.querySelector("#b3 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
-		document.querySelector("#b3 img").style.display = "block";
-		
-	}
+
+  if (b3filled == false) {
+    
+    if (turn == 1) {
+      // set the <div>'s inner <img> source to the O image, then make it display visibly 
+      document.querySelector("#b3 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
+      document.querySelector("#b3 img").style.display = "block";
+      turn = 2;
+      b3filled = true;
+
+    } else {
+      document.querySelector("#b3 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
+      document.querySelector("#b3 img").style.display = "block";
+      turn = 1;
+    }
+    
+  } else {
+    
+  }
+ 
+}
+  
+  
   
   document.querySelector("#c3").onclick = function() {
-		
-		// set the <div>'s inner <img> source to the O image, then make it display visibly 
-		document.querySelector("#c3 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
-		document.querySelector("#c3 img").style.display = "block";
-		
-	}
-	
-} // end of window.onload function
 
+  if (c3filled == false) {
+    
+    if (turn == 1) {
+      // set the <div>'s inner <img> source to the O image, then make it display visibly 
+      document.querySelector("#c3 img").src = "https://img.icons8.com/ios-filled/50/000000/x.png";
+      document.querySelector("#c3 img").style.display = "block";
+      turn = 2;
+      c3filled = true;
+
+    } else {
+      document.querySelector("#c3 img").src = "https://img.icons8.com/ios-filled/50/000000/o.png";
+      document.querySelector("#c3 img").style.display = "block";
+      turn = 1;
+    }
+    
+  } else {
+    
+  }
+ 
+}
+}
