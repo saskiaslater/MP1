f172f3182c143f53c6cedd9a18bf6fb69372c326
marleyschneider <marley.schneider@wsu.edu>
2021-12-13 19:25:20 +0000

    java

--- scripts2.js
+++ scripts2.js
@@ -20,7 +20,20 @@ function loadDatabase() {
 
     } else if ((this.readyState == 4) && (this.status != 200)) { // if .readyState is 4, the process is done; and if .status is NOT 200, output the error into the console
 
-      console.log("Error: " + this.responseText);
+      console.log("Output: " + this.responseText);
+      
+      testresponse = '[{"id":"6","appt":"2021-12-10 20:41:25","player_mark":"X","player_space":"A1"},{"id":"7","appt":"2021-12-10 20:41:32","player_mark":"O","player_space":"B2"},{"id":"8","appt":"2021-12-10 21:58:25","player_mark":"O","player_space":"C3"}]';
+      
+      incomingObject = JSON.parse(testresponse);
+      console.log(incomingObject);
+      
+      incomingObject[0].player_mark
+      incomingObject[0].player_space
+      
+      for (let i = 0, i < 5; i++) {
+        text += "" + i + "<br>";
+        
+      }
 
     }
 
